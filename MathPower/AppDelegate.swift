9caf9c934b9c2b5bfa034e5c26094108eccbe6ad
Aleksandr Ivashchenko <aleksandr.ivashchenko@sigma.software>
2017-12-05 16:47:11 +0200

    develop > Remove unnecessary code in App Delegate

--- MathPower/AppDelegate.swift
+++ MathPower/AppDelegate.swift
@@ -8,23 +8,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        let window = UIWindow(frame: UIScreen.main.bounds)
 
-        let viewController = ResultsViewController(
-            title: "Results",
-            summary: "You answered 1/2",
-            answers: [PresentableAnswer(calculation: "1+1",
-                                        answer: "2",
-                                        wrongAnswer: nil),
-                      PresentableAnswer(calculation: "2+2",
-                                        answer: "4",
-                                        wrongAnswer: "6")])
-            
-        _ = viewController.view
-        window.rootViewController = viewController
-        self.window = window
-        window.makeKeyAndVisible()
-
+        
+        
         return true
     }
 
